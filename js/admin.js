85cc46ae469983b92132976a72a1b2c22bed3b93
Phil <philfeiteng@gmail.com>
2021-01-22 21:58:49 -0800

    create localstorage item

--- js/admin.js
+++ js/admin.js
@@ -2,41 +2,40 @@
 // arr[0].innerHTML = "hello";
 // arr[1].innerHTML = "hello";
 
-// const item = {
-//     id: 1,
-//     problem: "1 + 1 = ",
-//     options: {
-//         a: {
-//             opt: "A",
-//             answer: "1"
-//         },
-//         b: {
-//             opt: "B",
-//             answer: "2"
-//         },
-//         c: {
-//             opt: "C",
-//             answer: "3"
-//         },
-//         d: {
-//             opt: "D",
-//             answer: "4"
-//         }   
-//     },
-//     key: "b"
-// };
+const item = {
+    id: 1,
+    problem: "1 + 1 = ",
+    options: {
+        a: {
+            opt: "A",
+            answer: "1"
+        },
+        b: {
+            opt: "B",
+            answer: "2"
+        },
+        c: {
+            opt: "C",
+            answer: "3"
+        },
+        d: {
+            opt: "D",
+            answer: "4"
+        }   
+    },
+    key: "b"
+};
 
-function item(id, problem, options, key) {
-    this.id = id;
-    this.problem = problem;
-    this.options = options;
-    this.key = key;
-}
+// function item(id, problem, options, key) {
+//     this.id = id;
+//     this.problem = problem;
+//     this.options = options;
+//     this.key = key;
+// }
 
-// localStorage.setItem("q1", JSON.stringify(item));
-// const q = localStorage.getItem("q1");
+localStorage.setItem("q1", JSON.stringify(item));
+const q = localStorage.getItem("q1");
 
-let e = document.getElementById('box')
-
-console.log(e.lastElementChild.previousElementSibling);
+console.log(q);
+// console.log(e.lastElementChild.previousElementSibling);
 
