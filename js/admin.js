3d43f830de9850ead40957b2067ad7f14dad1a6c
Phil <philfeiteng@gmail.com>
2021-01-23 15:31:51 -0800

    implement Add

--- js/admin.js
+++ js/admin.js
@@ -22,30 +22,19 @@
 //     key: "b"
 // };
 
-// let optA = new Option("A", "1");
-// let optB = new Option("B", "2");
-// let optC = new Option("C", "3");
-// let optD = new Option("D", "4");
-
-// let options = new Options(optA, optB, optC, optD);
-
-// let item = new Item(1, "1 + 1 = ", options, "b");
-
-
-// localStorage.setItem("q1", JSON.stringify(item));
-// const q = localStorage.getItem("q1");
-
-// console.log(q);
-// console.log(e.lastElementChild.previousElementSibling);
+// the number of answers for each question
+const NUM_OF_OPTIONS = 4;
 
 let items = [];
 let id = 0;
 
+// constructor for Option object
 function Option(opt, answer) {
     this.opt = opt;
     this.answer = answer;
 }
 
+// constructor for Options object, which contain 4 Option obj 
 function Options(a, b, c, d) {
     this.a = a;
     this.b = b;
@@ -53,6 +42,7 @@ function Options(a, b, c, d) {
     this.d = d;
 }
 
+// construcor for Item object
 function Item(id, problem, options, key) {
     this.id = id;
     this.problem = problem;
@@ -60,14 +50,19 @@ function Item(id, problem, options, key) {
     this.key = key;
 }
 
-
-
+// checks which radio button among a, b, c, d is checked
+// returns an object { option, boolean }
 function whichChkd() {
-    let optA = document.getElementById('optA').checked;
-    let optB = document.getElementById('optB').checked;
-    let optC = document.getElementById('optC').checked;
-    let optD = document.getElementById('optD').checked;
 
+    let opts = [];
+    let res;
+
+    for (var i = 0; i < NUM_OF_OPTIONS; ++i) {
+        res = String.fromCharCode(65 + i);
+        let id = "opt" + res;
+        opts[i] = document.getElementById(id).checked;
+    }
+      
     if(optA){
         console.log("A");
         return {opt: "A", chkd: optA};
@@ -85,34 +80,36 @@ function whichChkd() {
         return {opt: "D", chkd: optD};
     }
     else {
-        window.alert("MUST SET A KEY!");
-        return {opt: "x", chkd: false};
+        window.alert("A key must be assigned");
+        return {opt: "F", chkd: false};
     }
 }
 
+// a helper function for instantiation of an Item object
+// returns an Item object
 function createItem() {
-    const optA = "A";
-    const optB = "B";
-    const optC = "C";
-    const optD = "D";
 
     let chkd = whichChkd();
+    let answers = []; // stores 4 answers of a question, e.g. ( blah blah... )
+    let opts = []; // stores 4 options of a question, e.g. ( A. blah blah.... )
 
     if(chkd) { 
-        let problem = document.getElementById('problem').value;
-        let answerA = document.getElementById('answerA').value;
-        let answerB = document.getElementById('answerB').value;
-        let answerC = document.getElementById('answerC').value;
-        let answerD = document.getElementById('answerD').value;
-        
-        let a = new Option(optA, answerA);
-        let b = new Option(optB, answerB);
-        let c = new Option(optC, answerC);
-        let d = new Option(optD, answerD);
 
-        let options = new Options(a,b,c,d);
+        // the question
+        let problem = document.getElementById('problem').value;
+
+        // dynamically assign a variable for one option
+        for (var i = 0; i < NUM_OF_OPTIONS; ++i) {
+            res = String.fromCharCode(65 + i);
+            let id = "answer" + res;
+            answers[i] = document.getElementById(id).value;
+            opts[i] = new Option(res, answers[i]); // optionA == opts[0]
+        }
+        
+        let options = new Options(opts[0],opts[1],opts[2],opts[3]);
 
         ++id;
+
         let item = new Item(id, problem, options, chkd["opt"]);
 
         console.log(problem);
@@ -123,61 +120,84 @@ function createItem() {
     }
 }
 
-
+// displays a quiz question above the form
 function viewMCQ(item) {
+
     let problem = item["problem"];
 
-    let a1 = item["options"]['a']['answer'];
-    let a2 = item["options"]['b']['answer'];
-    let a3 = item["options"]['c']['answer'];
-    let a4 = item["options"]['d']['answer'];
+    let ax = [];
+    let paras = [];
+    let nodes = [];
 
+    for (var i = 0; i < NUM_OF_OPTIONS; ++i) {
+        res = String.fromCharCode(97 + i);
+        ax[i] = item["options"][res]['answer'];
+    }
+    
     let e = document.getElementById("box");
     let sibling = e.lastElementChild.previousElementSibling;
 
     if(sibling){
         console.log(sibling);
+
+        sibling.insertAdjacentHTML("afterend", "<div><p>" + problem + "</p></div>");
+
     } else {
-        let para0 = document.createElement("P");
-        let para1 = document.createElement("P");
-        let para2 = document.createElement("P");
-        let para3 = document.createElement("P");
-        let para4 = document.createElement("P");
-        
-        let node0 = document.createTextNode("Question:" + problem);
-        let node1 = document.createTextNode("A. " + a1);
-        let node2 = document.createTextNode("B. " + a2);
-        let node3 = document.createTextNode("C. " + a3);
-        let node4 = document.createTextNode("D. " + a4);
+        /**
+         * structure we need to dynamically create
+         *  <div>
+         *      <p>textNode</p>
+         *      <p>textNode</p>
+         *      <p>textNode</p>
+         *      <p>textNode</p>
+         *  </div>
+         */
 
-
-        para0.appendChild(node0);
-        para1.appendChild(node1);
-        para2.appendChild(node2);
-        para3.appendChild(node3);        
-        para4.appendChild(node4);
-
+        // we creates the container <div> tag
         let myDiv = document.createElement("DIV");
-        myDiv.appendChild(para0);
-        myDiv.appendChild(para1);
-        myDiv.appendChild(para2);
-        myDiv.appendChild(para3);
-        myDiv.appendChild(para4);
 
+        // we need to create 5 <p> tags. one for the question, 4 for the answers
+        for (var i = 0; i < NUM_OF_OPTIONS + 1; ++i) {
+
+            paras[i] = document.createElement("P"); 
+
+            // if it is the first <p> tag we create a node for the first <p> tag
+            if(i === 0) {
+                nodes[i] = document.createTextNode("Question:" + problem);
+            } else { // else we create 4 nodes for the answers
+                res = String.fromCharCode(64 + i); 
+                nodes[i] = document.createTextNode(res+ ". " + ax[i - 1]);
+            }
+
+            // we append each text node to the <p> 
+            paras[i].appendChild(nodes[i]);
+
+            // we append the 1 question <p> elements and 4 answer <p> elements to the <div>
+            myDiv.appendChild(paras[i]);
+        }
+
+        // adds the structure to our html file
         let box = document.getElementById("box");
         box.insertBefore(myDiv, box.childNodes[0]);
     }
-
-    
-
 }
 
+// when Add btn is clicked
 function addBtnHandler() {
     let item =  createItem();
     let k = id.toString();
     localStorage.setItem(k, JSON.stringify(item));
     const q = localStorage.getItem("q");
     viewMCQ(item);
+
+    // this is a bad way for clearing all the fields
+    // we can use a loop to clear it.
+    document.getElementById("problem").value = "";
+    document.getElementById("answerA").value = "";
+    document.getElementById("answerB").value = "";
+    document.getElementById("answerC").value = "";
+    document.getElementById("answerD").value = "";
+
 }
 
 document.getElementById("add").onclick = addBtnHandler;
